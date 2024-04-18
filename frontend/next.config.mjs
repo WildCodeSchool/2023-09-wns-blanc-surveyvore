dc4446adcb1063df4b2a658f59b02904f06973a8
6sous <cyrille.aquilina@gmail.com>
2024-04-18 12:28:59 +0200

    create docker-compose.prod

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    compiler: {
+        reactRemoveProperties: true,
+    },
 };
 
 export default nextConfig;
-
