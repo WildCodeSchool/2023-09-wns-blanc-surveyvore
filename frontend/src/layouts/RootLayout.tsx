c12bbd03235d9244d4e9a703f1faeec31d786bda
6sous <cyrille.aquilina@gmail.com>
2024-05-15 15:22:37 +0200

    fixes

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,21 +1,17 @@
 import Head from "next/head";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
-
-    return (
-        <>
-            <Head>
-                <title>Surveyvore</title>
-                <meta name="description" content="Surveyvore" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.svg" />
-            </Head>
-            {children}
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Surveyvore</title>
+        <meta name="description" content="Surveyvore" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      {children}
+    </>
+  );
 }
 
 export default RootLayout;
