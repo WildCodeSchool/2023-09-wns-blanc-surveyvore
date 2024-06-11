3d2a692719661de2e15798ae17bd9f7488e71cb3
6sous <cyrille.aquilina@gmail.com>
2024-06-11 15:22:30 +0200

    add custom header for any pages

--- frontend/src/layouts/NavLayout.tsx
+++ frontend/src/layouts/NavLayout.tsx
@@ -1,12 +1,36 @@
 import NavHeader from "@/components/NavHeader/NavHeader";
 
-function NavLayout({ children }: { children: React.ReactNode }) {
+function NavLayout({
+  children,
+  newSurvey,
+  backToForms,
+  badge,
+  signOut,
+  profile,
+  publish,
+  signInOrSignUp,
+}: {
+  children: React.ReactNode;
+  newSurvey?: boolean;
+  backToForms?: boolean;
+  badge?: boolean;
+  signOut?: boolean;
+  profile?: boolean;
+  publish?: boolean;
+  signInOrSignUp?: boolean;
+}) {
   return (
     <>
-      <NavHeader />
-      <main className="main main-nav">
-        {children}
-      </main>
+      <NavHeader
+        newSurvey={newSurvey}
+        backToForms={backToForms}
+        badge={badge}
+        signOut={signOut}
+        profile={profile}
+        publish={publish}
+        signInOrSignUp={signInOrSignUp}
+      />
+      <main className="main main-nav">{children}</main>
     </>
   );
 }
